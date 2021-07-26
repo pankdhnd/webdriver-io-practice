5e323628fb8686f630e1c1d91ea4b2d2326de887
pankdhnd <pank.dhnd@gmail.com>
2021-07-26 16:45:45 +0530

    more practice

--- basic.js
+++ basic.js
@@ -1,3 +1,5 @@
+
+
 //print to console
 console.log("Hello World")
 
@@ -11,51 +13,50 @@ let a = 2
 let b = 2
 const flag = true
 
-console.log(a+b)
+console.log(a + b)
 
 //lets define a string
 let str = "test string"
 
 //check data type of variable
-console.log(typeof(str))
+console.log(typeof (str))
 
 //If else
 let c = 5
-if (c == 5){
-    console.log("C is 5")
-}
-else {
-    console.log("C is not 5")
+if (c == 5) {
+	console.log("C is 5")
+} else {
+	console.log("C is not 5")
 }
 
 //While loop
 var d = 0
-while(d <=3 ){
-   //console.log(d)
-    d++
+while (d <= 3) {
+	//console.log(d)
+	d++
 }
 
 //Do while loop
 d = 0
-do{
-    //console.log(d)
-    d++
-}while(d < 3);
+do {
+	//console.log(d)
+	d++
+} while (d < 3);
 
 //For loop
-for (let i=0;i<3;i++){
-    console.log(i)
+for (let i = 0; i < 3; i++) {
+	console.log(i)
 }
 
 //Arrays
 var marks = Array(5)
-new Array(10,20,30,40,50)
+new Array(10, 20, 30, 40, 50)
 
 //OR simply
-var marks = new Array(10,20,30,40,50)
+var marks = new Array(10, 20, 30, 40, 50)
 
 //OR even more simple
-var marks = [10,20,30,40,50]
+var marks = [10, 20, 30, 40, 50]
 console.log(marks[3])
 console.log("length of array: " + marks.length)
 //Add new element to array
@@ -66,12 +67,12 @@ console.log("Deleted an element from the array: " + marks)
 marks.unshift(0)
 console.log("Added an element at the start of the array: " + marks)
 console.log(marks.includes(50)) //checks if array includes/consits of a specified element and prints true/false result to screen
-console.log("Slied array: " + marks.slice(2,6)) //slices and array and prints results to console
+console.log("Slied array: " + marks.slice(2, 6)) //slices and array and prints results to console
 
 //sum all elements of an array using for loop
 var sum = 0
-for (i=0;i<marks.length;i++){
-    sum = sum + marks[i]
+for (i = 0; i < marks.length; i++) {
+	sum = sum + marks[i]
 }
 console.log("Sum of elements within the array: " + sum)
 
@@ -88,13 +89,14 @@ let sum = numbers.reduce(function (accumulator, current) {
 console.log(sum); // 6
 
 */
-console.log(marks.reduce((temp,mark)=>temp+mark,0))
+console.log(marks.reduce((temp, mark) => temp + mark, 0))
 
 /* Using filters, find the even numbers in the array. In below example, we have spcified stored_element to be used as a temporary variable which stores the single
 value of the array during each iteration. In the next part after =>, we are performing core loging to calucate whether the nubmer is even by checking its moderator is 
 0 or not.
 */
-var numbers = [15,85,9,45,36,98,25,65]
-var filteredNumbers = numbers.filter(stored_element=>stored_element%2==0)
+var numbers = [15, 85, 9, 45, 36, 98, 25, 65]
+var filteredNumbers = numbers.filter(stored_element => stored_element % 2 == 0)
 
 console.log("Filtered array: " + filteredNumbers)
+
